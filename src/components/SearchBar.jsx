567e7c7810dff4951afd1a8db3912ed69f766833
Patrick Duvall <duvallp208@gmail.com>
2021-01-12 12:10:11 -0700

    Add video list, video item, initial video detail

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
 
 class SearchBar extends React.Component {
+
+  state = {term: ''}
+
+  onInputChange = (e) => {
+    this.setState({ term: e.target.value})
+  }
+
+  onFormSubmit = (e) => {
+    e.preventDefault()
+
+    this.props.onTermSubmit(this.state.term)
+  }
+
   render () {
-    return <div>SearchBar</div>
+    return <div className="search-bar ui segment">
+      <form onSubmit={this.onFormSubmit} className='ui form'>
+        <div className="field">
+          <label>VideoSearch</label>
+          <input type={this.state.term}
+          onChange={this.onInputChange}/>
+        </div>
+
+      </form>
+    </div>
   }
 }
 
